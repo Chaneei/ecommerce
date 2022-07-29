ff6b56695251f4d1807ed29a53baf99f3b528108
Chani <lsc90726@gmail.com>
2022-07-29 16:09:27 +0900

    6. Styled Component Hover&Animation

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,6 +4,22 @@ import {
   SearchOutlined,
   ShoppingCartOutlined,
 } from "@material-ui/icons";
+
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+`;
+
 const Container = styled.div`
   flex: 1;
   margin: 5px;
@@ -14,6 +30,10 @@ const Container = styled.div`
   align-items: center;
   background-color: #f5fbfd;
   position: relative;
+
+  &:hover ${Info} {
+    opacity: 1;
+  }
 `;
 
 const Circle = styled.div`
@@ -27,18 +47,7 @@ const Image = styled.img`
   height: 75%;
   z-index: 2;
 `;
-const Info = styled.div`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  background-color: rgba(0, 0, 0, 0.2);
-  z-index: 3;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
+
 const Icon = styled.div`
   width: 40px;
   height: 40px;
