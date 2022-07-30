f175cb5fc4e3b331ce8b5043d4ceb7048d670822
Chani <lsc90726@gmail.com>
2022-07-30 21:03:19 +0900

    9. Category Page

---
+++ src/pages/ProductList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Navbar from "../components/Navbar";
+
+const Container = styled.div``;
+const Title = styled.h1``;
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const Filter = styled.div``;
+
+const ProductList = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Title>Dresses</Title>
+      <FilterContainer>
+        <Filter>filter</Filter>
+        <Filter>filter2</Filter>
+      </FilterContainer>
+    </Container>
+  );
+};
+
+export default ProductList;
