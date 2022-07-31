d4c0bbb3e75c2e3de4e35d8d8b2b827c4bc80c06
Chani <lsc90726@gmail.com>
2022-07-31 19:20:01 +0900

    12. Login&Register Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from "./pages/Home";
 import Product from "./pages/Product";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 function App() {
   return (
     <>
-      <Product />
+      <Login />
     </>
   );
 }
