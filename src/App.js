da697c07615a8b4cb2e75c068ea3f1654fa63ad2
Chani <lsc90726@gmail.com>
2022-08-02 13:44:00 +0900

    13.Shopping Cart Page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 function App() {
   return (
     <>
-      <Login />
+      <Cart />
     </>
   );
 }
